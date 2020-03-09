596ecd687ab561e43399a7d4ce7dad5663645f89
Sara Laffin <sarala@umich.edu>
2020-03-09 14:21:52 -0400

    more merging

--- src/App.js
+++ src/App.js
@@ -6,11 +6,8 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">hello world!</header>
-<<<<<<< HEAD
       <div>Zack Friendly</div>
-=======
       <div>Sara</div>
->>>>>>> Sara
     </div>
   );
 }
