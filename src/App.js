50c72ee0ef82fb87914448bc7c4417e0b78b5856
Sara Laffin <sarala@umich.edu>
2020-03-10 13:48:32 -0400

    set up links and first route

--- src/App.js
+++ src/App.js
@@ -1,19 +1,47 @@
-import React from "react";
-import logo from "./logo.svg";
+import React, { Component } from "react";
+import { Route, Link } from "react-router-dom";
 import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">hello world!</header>
-      <div>Zack Friendly</div>
+class App extends Component {
+  //code below copied from national parks lab
+  // constructor() {
+  //   super();
 
-      <h1>Sarah</h1>
+  //   this.state = {
+  //     parks: null
+  //   };
+  // }
+  // componentDidMount() {
+  // if (!this.state.parks) {
+  //   let url =
+  //     "https://developer.nps.gov/api/v1/parks?limit=50&fields=images,addresses&api_key=KDAvx939Yj9FsDsKrBSHeZi7BMzZHUqpQdLU5tfV";
 
-      <p>Norman</p>
-      <div>Sara</div>
-    </div>
-  );
+  //   fetch(url)
+  //     .then(res => res.json())
+  //     .then(res => {
+  //       this.setState({ parks: res.data });
+  //     });
+  // }
+  // }
+  render() {
+    return (
+      <div>
+        <header>
+          <Link to="/">Posts</Link>
+          <Link to="/dogs">Dog Breeds</Link>
+          <Link to="/cats">Cat Breeds</Link>
+          <Link to="/newpet">Add a Pet</Link>
+        </header>
+        <main>
+          <Route
+            path="/"
+            exact
+            component={routerProps => <div>This is the home page!</div>}
+          />
+        </main>
+      </div>
+    );
+  }
 }
 
 export default App;
