c518f38ab31c2808ac3c7cf9d8d447a390c85aa0
Zack Friendly <friendly.wisconsin@gmail.com>
2020-03-11 17:08:48 -0400

    Built CatBreeds page, need to touch up CSS

--- src/components/CatBreeds/CatBreeds.js
+++ src/components/CatBreeds/CatBreeds.js
@@ -14,15 +14,75 @@ class CatBreeds extends Component {
       selectedBreed: null
     };
   }
+  renderStats = () => {
+    if (this.state.selectedBreed) {
+      return (
+        <div>
+          <div className="CatBreeds__info-title">
+            {this.state.selectedBreed.breedName}
+          </div>
+          <div className="CatBreeds__info-property">
+            <span className="CatBreeds__info-key">
+              Affectionate with Family:{" "}
+            </span>
+            {this.state.selectedBreed["Affectionate with Family"]}
+          </div>
+          <div className="CatBreeds__info-property">
+            <span className="CatBreeds__info-key">Amount of Shedding: </span>
+            {this.state.selectedBreed["Amount of Shedding"]}
+          </div>
+          <div className="CatBreeds__info-property">
+            <span className="CatBreeds__info-key">Easy to Groom: </span>
+            {this.state.selectedBreed["Easy to Groom"]}
+          </div>
+          <div className="CatBreeds__info-property">
+            <span className="CatBreeds__info-key">
+              Friendly Toward Strangers:
+            </span>
+            {this.state.selectedBreed["Friendly Toward Strangers"]}
+          </div>
+          <div className="CatBreeds__info-property">
+            <span className="CatBreeds__info-key">Intelligence: </span>
+            {this.state.selectedBreed.Intelligence}
+          </div>
+          <div className="CatBreeds__info-property">
+            <span className="CatBreeds__info-key">General Health:</span>
+            {this.state.selectedBreed["General Health"]}
+          </div>
+          <div className="CatBreeds__info-property">
+            <span className="CatBreeds__info-key">Kid Friendly: </span>
+            {this.state.selectedBreed["Kid Friendly"]}
+          </div>
+          <div className="CatBreeds__info-property">
+            <span className="CatBreeds__info-key">Pet Friendly: </span>
+            {this.state.selectedBreed["Pet Friendly"]}
+          </div>
+          <div className="CatBreeds__info-property">
+            <span className="CatBreeds__info-key">
+              Potential for Playfulness:{" "}
+            </span>
+            {this.state.selectedBreed["Potential for Playfulness"]}
+          </div>
+          <div className="CatBreeds__info-property">
+            <span className="CatBreeds__info-key">Tendency to Vocalize: </span>
+            {this.state.selectedBreed["Tendency to Vocalize"]}
+          </div>
+        </div>
+      );
+    } else {
+      return null;
+    }
+  };
+
   clickHandle = e => {
     console.log(this.state.breeds);
     let catObject = this.state.breeds.filter(el => {
       if (el.breedName === e.target.innerText) {
-        return <div className="test">HELLO</div>;
+        return <div>HELLO</div>;
       }
     });
-    this.setState({ selectedBreed: catObject.breedName });
-    console.log(catObject);
+    this.setState({ selectedBreed: catObject[0] });
+    console.log(this.state.selectedBreed);
   };
 
   //method to populate cards
@@ -56,9 +116,16 @@ class CatBreeds extends Component {
       return (
         <div>
           <div className="CatBreeds___container">{this.populateBreeds()}</div>
-          <div className="hello">{this.state.selectedBreed}</div>
+          <div className="CatBreeds__info-container">{this.renderStats()}</div>
         </div>
       );
+      // } else if (this.state.breeds && this.state.selectedBreed) {
+      //   return (
+      //     <div>
+      //       <div className="CatBreeds___container">{this.populateBreeds()}</div>
+      //       <div className="test">{this.state.selectedBreed[0].breedName}</div>
+      //     </div>
+      //   );
     } else {
       return <div>Loading!</div>;
     }
