69878ccbd49e88aada07fccf9a130e249492f639
Norman Calhoun <ndc1963@gmail.com>
2020-03-12 14:20:34 -0400

    changed issues

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
-import React from "react";
-import logo from "./logo.svg";
+import React, { Component } from "react";
+import { Route, Link } from "react-router-dom";
 import "./App.css";
+import HomePage from "./components/HomePage/HomePage";
+import DogBreeds from "./components/DogBreeds/DogBreeds";
+import CatBreeds from "./components/CatBreeds/CatBreeds";
+import AddPet from "./components/AddPet/AddPet";
+import Header from "./components/Header/Header";
+import Footer from "./components/Footer/Footer";
+import PetProfile from "./components/PetProfile/PetProfile";
 
+<<<<<<< HEAD
 function App() {
   return (
     <div className="App">
@@ -17,6 +25,44 @@ function App() {
       <div>Sara</div>
     </div>
   );
+=======
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <Header />
+        <main className="main__container">
+          <Route
+            path="/"
+            exact
+            render={routerProps => <HomePage {...routerProps} />}
+          />
+          <Route
+            path="/dogs"
+            exact
+            render={routerProps => <DogBreeds {...routerProps} />}
+          />
+          <Route
+            path="/cats"
+            exact
+            render={routerProps => <CatBreeds {...routerProps} />}
+          />
+          <Route
+            path="/addPet"
+            exact
+            render={routerProps => <AddPet {...routerProps} />}
+          />
+            <Route
+            path="/pet/:id"
+            exact
+            render={routerProps => <PetProfile {...routerProps} />}
+          />
+        </main>
+        {/* <Footer /> */}
+      </div>
+    );
+  }
+>>>>>>> ed35783b42d74dfe2fac5b532471d46f7e9407b9
 }
 
 export default App;
---
+++ src/components/Card/Card.js
@@ -0,0 +1,64 @@
+import React, { Component } from "react";
+import "../../App.css";
+import "./Card.css";
+import "../Button/Button.css";
+import Like from "../Like/Like";
+import Button from "../Button/Button";
+import Axios from "axios";
+import getAPI from "../../util/util";
+import { Link } from "react-router-dom";
+
+class Card extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      numberOfLikes: this.props.numberOfLikes || 0,
+      liked: false
+    };
+  }
+  clickHandle = () => {
+    if (!this.state.liked) {
+      const currentLikes = this.state.numberOfLikes + 1;
+      this.setState({
+        liked: true,
+        numberOfLikes: currentLikes
+      });
+      Axios.put(`${getAPI()}pet/${this.props._id}`, {
+        numberOfLikes: currentLikes
+      });
+    }
+  };
+  render() {
+    return (
+      <div className="card__background">
+        <div className="card__feed">
+          <div className="card__container">
+            <div
+              className="card__container-image"
+              style={{ backgroundImage: `url(${this.props.image})` }}
+            ></div>
+            <div className="card__container-caption">
+              <span className="card__container-username">
+                @{this.props.petName}:
+              </span>{" "}
+              {this.props.caption}
+            </div>
+            <div className="card__container-links">
+              <div className="card-likes">
+                <div className="card__container-like">
+                  <Like active={this.state.liked} onClick={this.clickHandle} />
+                </div>
+                <div>{this.state.numberOfLikes} Likes</div>
+              </div>
+              <Link to={`/pet/${this.props._id}`}>
+                <Button label="See Post" type="lprimshad" />
+              </Link>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Card;
