488eb23347c3975eeaeb6a5ff34d3b73d076d875
Zack Friendly <friendly.wisconsin@gmail.com>
2020-03-12 10:45:43 -0400

    Background color changes

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -27,7 +27,7 @@ class Card extends Component {
   };
   render() {
     return (
-      <div>
+      <div className="card__background">
         <div className="card__feed">
           <div className="card__container">
             <div
