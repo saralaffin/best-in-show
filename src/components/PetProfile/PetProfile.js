d35e44fcc9da3d4498ff32545862bbce5977aa86
Zack Friendly <friendly.wisconsin@gmail.com>
2020-03-12 14:24:01 -0400

    Fixed styling on PetProfile page

--- src/components/PetProfile/PetProfile.js
+++ src/components/PetProfile/PetProfile.js
@@ -1,7 +1,7 @@
 import React, { Link, Component } from "react";
 import PropTypes from "prop-types";
 import getAPI from "../../util/util";
-
+import "./PetProfile.css";
 import axios from "axios";
 import Card from "../Card/Card";
 import HomePage from "../HomePage/HomePage";
@@ -37,22 +37,28 @@ class PetProfile extends Component {
     if (this.state.petDetails && !this.state.deleted) {
       return (
         <div>
-          <div className="card__container">
+          <div className="pet__container">
             <div
-              className="card__container-image"
+              className="pet__container-image"
               style={{ backgroundImage: `url(${this.state.petDetails.image})` }}
             ></div>
-            <div className="card__container-caption">
-              <span className="card__container-username">
+            <div className="pet__container-caption">
+              <span className="pet__container-username">
                 @{this.state.petDetails.petName}:
               </span>{" "}
               {this.state.petDetails.caption}
             </div>
-            <div>
-              <p>Breed: {this.state.petDetails.breed}</p>
-              <p>Age: {this.state.petDetails.age}</p>
+            <div className="pet__container-more-stats">
+              <p className="pet__container-breed">
+                <span>Breed: </span>
+                {this.state.petDetails.breed}
+              </p>
+              <p className="pet__container-age">
+                <span>Age: </span>
+                {this.state.petDetails.age}
+              </p>
             </div>
-            <div className="card__container-links">
+            <div className="pet__container-links">
               <div>
                 <span>{this.state.petDetails.numberOfLikes} Likes</span>
               </div>
